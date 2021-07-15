62c30a2311912db732e965bac07a5facf85fef6a
lazio@wataidesign.com <lazio@wataidesign.com>
2021-07-15 18:38:59 +0800

    110-0715-3

--- js/javascript.js
+++ js/javascript.js
@@ -49,3 +49,15 @@ $(function(){
         }
     })
 })
+
+// 圖片輪播
+$(document).on('ready', function () {
+	$(".single-item").slick({
+		dots: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3500,
+		arrows: false,
+	})
+})

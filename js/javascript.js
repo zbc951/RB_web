7b5792eecbd2693d877068a94fbd6c61fbd58d0e
lazio@wataidesign.com <lazio@wataidesign.com>
2021-07-23 10:54:01 +0800

    110-0723-4

--- js/javascript.js
+++ js/javascript.js
@@ -5,20 +5,20 @@ $(function(){
     var size2 = $('header .bottom ul');
     var size3 = $('header article');
     var size4 = $('.dicoration.d2 div:nth-of-type(2)');
-    if(wWD > 1710) {
+    if(wWD > 1720) {
         $(size1).css('width', (String(267) + "px"));
         $(size2).css('padding-right', (String(110) + "px"));
         $(size3).css('width', (String(1460) + "px"));
         $(size4).css('width', (String(351) + "px"));
-    }else if(wWD <= 1710 && wWD > 1510) {
-        $(size1).css('width', (String(wWD - 1443) + "px"));
-        $(size2).css('padding-right', (String(wWD - 1600) + "px"));
-        $(size3).css('width', (String(wWD - 250) + "px"));
+    }else if(wWD <= 1720 && wWD > 1510) {
+        $(size1).css('width', (String(wWD - 1463) + "px"));
+        $(size2).css('padding-right', (String(wWD - 1620) + "px"));
+        $(size3).css('width', (String(wWD - 270) + "px"));
     }else if(wWD <= 1510 && wWD > 1280) {
         $(size1).css('width', (String(68) + "px"));
         $(size2).css('padding-right', (String(0) + "px"));
-        $(size3).css('width', (String(wWD - 250) + "px"));
-        $(size4).css('width', (String(wWD - 1170) + "px"));
+        $(size3).css('width', (String(wWD - 270) + "px"));
+        $(size4).css('width', (String(wWD - 1190) + "px"));
     }else {
         $(size1).css('width', (String(68) + "px"));
         $(size2).css('padding-right', (String(0) + "px"));
@@ -32,20 +32,20 @@ $(function(){
         var size2 = $('header .bottom ul');
         var size3 = $('header article');
         var size4 = $('.dicoration.d2 div:nth-of-type(2)');
-        if(wWD > 1710) {
+        if(wWD > 1720) {
             $(size1).css('width', (String(267) + "px"));
             $(size2).css('padding-right', (String(110) + "px"));
             $(size3).css('width', (String(1460) + "px"));
             $(size4).css('width', (String(351) + "px"));
-        }else if(wWD <= 1710 && wWD > 1510) {
-            $(size1).css('width', (String(wWD - 1443) + "px"));
-            $(size2).css('padding-right', (String(wWD - 1600) + "px"));
-            $(size3).css('width', (String(wWD - 250) + "px"));
+        }else if(wWD <= 1720 && wWD > 1510) {
+            $(size1).css('width', (String(wWD - 1463) + "px"));
+            $(size2).css('padding-right', (String(wWD - 1620) + "px"));
+            $(size3).css('width', (String(wWD - 270) + "px"));
         }else if(wWD <= 1510 && wWD > 1280) {
             $(size1).css('width', (String(68) + "px"));
             $(size2).css('padding-right', (String(0) + "px"));
-            $(size3).css('width', (String(wWD - 250) + "px"));
-            $(size4).css('width', (String(wWD - 1170) + "px"));
+            $(size3).css('width', (String(wWD - 270) + "px"));
+            $(size4).css('width', (String(wWD - 1190) + "px"));
         }else {
             $(size1).css('width', (String(68) + "px"));
             $(size2).css('padding-right', (String(0) + "px"));
